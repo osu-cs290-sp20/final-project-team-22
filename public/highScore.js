15094c80b0e2f5fee5c7bdf98729ffb6f797a8cd
David Gasper <gasperd@oregonstate.edu>
2020-06-10 16:13:33 -0700

    Sorta-working MongoDB transactions

--- public/highScore.js
+++ public/highScore.js
@@ -1,4 +1,29 @@
 var topScore = document.getElementsByClassName('score-icon').item(0);
 topScore.innerHTML='<img src="/images/topscore.png">';
 
+var saveButton = document.querySelector('.modal-accept-button');
 
+saveButton.addEventListener('click', function (event) {
+    var username = document.getElementById('username-input-box').value;
+    var score = document.getElementById('score-input-box').value;
+
+    if (username !== "" && score !== "") {
+        var http = new XMLHttpRequest();
+        var URL = '/highscores/new';
+        var userData = {
+            "name": username,
+            "score": score
+        };
+        http.open('POST', URL, true);
+        http.setRequestHeader("Content-Type", "application/json");
+
+        http.onchangestate = function () {
+            if (http.readyState === 4 && http.status === 200) {
+                console.log(this.responseText);
+            }
+         };
+
+        http.send(JSON.stringify(userData));
+        console.log(userData);
+    }
+});
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-
+var bodyParser = require('body-parser');
 var cityData = require('./cityData.json');
-
 //Temporary code for testing. Should be replaced with MongoDB calls in production
-var scoreData = require('./testScoreData.json');
+var scoreData;
 
 var app = express();
 
@@ -13,12 +12,24 @@ var port = process.env.PORT || 9000;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'mainPage'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://QxnWrw6QzWvQvTm8:<password>@cluster0-aqhqf.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const uri = "mongodb+srv://QxnWrw6QzWvQvTm8:W7Q3DBwwnJsFAYC@cluster0-aqhqf.mongodb.net/CS290?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("CS290").collection("CityGuess");
+
+  var highScores = collection.find({});
+  highScores.toArray(function (err, scores) {
+    if (err) {
+      res.status(500).send("Error fetching people from DB.");
+    } else {
+      console.log("Read from database successful");
+      console.log(scores);
+      scoreData = scores;
+    }
+  });
   // perform actions on the collection object
   client.close();
 });
@@ -51,6 +62,45 @@ app.get('/start', function(req, res){
 
 app.get('/highscores', function(req, res){
     res.status(200).render('highScore', { allScores: scoreData });
+});
+
+app.post('/highscores/new', function(req, res, next){
+    console.log("Data received");
+    if (req.body.name && req.body.score) {
+        var newScore = {
+            name: req.body.name,
+            score: req.body.score
+        };
+
+        const MongoClient = require('mongodb').MongoClient;
+        const uri = "mongodb+srv://QxnWrw6QzWvQvTm8:W7Q3DBwwnJsFAYC@cluster0-aqhqf.mongodb.net/CS290?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+        const collection = client.db("CS290").collection("CityGuess");
+        collection.insertOne(newScore, function (err, scores) {
+        if (err) {
+            res.status(500).send("Error accessing DB.");
+        } else {
+           console.log("Write to database successful");
+           var highScores = collection.find({});
+           highScores.toArray(function (err, scores) {
+           if (err) {
+               res.status(500).send("Error fetching people from DB.");
+           } else {
+              console.log("Read from database successful");
+              console.log(scores);
+              scoreData = scores;
+           }
+        });
+      }
+    });
+    // perform actions on the collection object
+    client.close();
+});
+    } else {
+        console.log(req);
+        res.status(400).send("Invalid request received");
+    }
 });
 
 //serves public files
