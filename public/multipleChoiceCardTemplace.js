177b86298fdd08b17e33412f72adac88057af4c8
David Gasper <gasperd@oregonstate.edu>
2020-06-11 07:55:40 -0700

    More tweaks

--- public/multipleChoiceCardTemplace.js
+++ public/multipleChoiceCardTemplace.js
@@ -8,12 +8,12 @@ templates['multipleChoiceCard'] = template({"compiler":[8,">= 4.3.0"],"main":fun
         return undefined
     };
 
-  return "<section class=\"mutipleChoiceBox\">\r\n    <div class=\"choicelist\">\r\n      <label for=\"one\">\r\n        <input type=\"checkbox\" id=\"one\" />"
+  return "<section class=\"mutipleChoiceBox\">\n    <div class=\"choicelist\">\n      <label for=\"one\">\n        <input type=\"checkbox\" id=\"one\" />"
     + alias4(((helper = (helper = lookupProperty(helpers,"city") || (depth0 != null ? lookupProperty(depth0,"city") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"city","hash":{},"data":data,"loc":{"start":{"line":4,"column":42},"end":{"line":4,"column":50}}}) : helper)))
-    + "</label>\r\n      <label for=\"two\">\r\n        <input type=\"checkbox\" id=\"two\" />"
+    + "</label>\n      <label for=\"two\">\n        <input type=\"checkbox\" id=\"two\" />"
     + alias4(((helper = (helper = lookupProperty(helpers,"city") || (depth0 != null ? lookupProperty(depth0,"city") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"city","hash":{},"data":data,"loc":{"start":{"line":6,"column":42},"end":{"line":6,"column":50}}}) : helper)))
-    + "</label>\r\n      <label for=\"three\">\r\n        <input type=\"checkbox\" id=\"three\" />"
+    + "</label>\n      <label for=\"three\">\n        <input type=\"checkbox\" id=\"three\" />"
     + alias4(((helper = (helper = lookupProperty(helpers,"city") || (depth0 != null ? lookupProperty(depth0,"city") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"city","hash":{},"data":data,"loc":{"start":{"line":8,"column":44},"end":{"line":8,"column":52}}}) : helper)))
-    + "</label>\r\n    </div>\r\n  </div>\r\n</section>";
+    + "</label>\n    </div>\n  </div>\n</section>";
 },"useData":true});
 })();
