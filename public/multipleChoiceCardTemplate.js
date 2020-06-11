e459697bfcaaa298d791434daa64b9fb466e484b
David Gasper <gasperd@oregonstate.edu>
2020-06-11 08:47:56 -0700

    Final styling tweaks

--- public/multipleChoiceCardTemplate.js
+++ public/multipleChoiceCardTemplate.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['multipleChoiceCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<section class=\"input\">\r\n        <div class=\"login\">\r\n            <input type=\"text\" id=\"city-input\" placeholder=\"Enter your guess here\">\r\n            <button id = \"submit-button\">Submit</button>\r\n        </div>\r\n</section>";
+    return "<section class=\"input\">\n        <div class=\"login\">\n            <input type=\"text\" id=\"city-input\" placeholder=\"Enter your guess here\">\n            <button id = \"submit-button\">Submit</button>\n        </div>\n</section>";
 },"useData":true});
 })();
--- public/scoreCardTemplate.js
+++ public/scoreCardTemplate.js
@@ -8,10 +8,10 @@ templates['scoreCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(con
         return undefined
     };
 
-  return "<article class=\"scoreCard\">\r\n    <div class=\"content\">\r\n        <div class=\"scoreContent\">\r\n            <div class=\"score-icon\">\r\n            </div>\r\n            <div class=\"userdata\">\r\n                <p class=\"username\">\r\n                    "
+  return "<article class=\"scoreCard\">\n    <div class=\"content\">\n        <div class=\"scoreContent\">\n            <div class=\"score-icon\">\n            </div>\n            <div class=\"userdata\">\n                <p class=\"username\">\n                    "
     + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":8,"column":20},"end":{"line":8,"column":28}}}) : helper)))
-    + "\r\n               </p>\r\n               <p class=\"scorevalue\">\r\n                    "
+    + "\n               </p>\n               <p class=\"scorevalue\">\n                    "
     + alias4(((helper = (helper = lookupProperty(helpers,"score") || (depth0 != null ? lookupProperty(depth0,"score") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"score","hash":{},"data":data,"loc":{"start":{"line":11,"column":20},"end":{"line":11,"column":29}}}) : helper)))
-    + "\r\n                </p>\r\n            </div>\r\n        </div>\r\n    </div>\r\n</article>\r\n";
+    + "\n                </p>\n            </div>\n        </div>\n    </div>\n</article>\n";
 },"useData":true});
 })();
